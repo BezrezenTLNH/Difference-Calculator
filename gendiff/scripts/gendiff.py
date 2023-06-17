99c5cc125547408b893811a4e8706fdda872e10c
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-06-17 12:35:55 +0200

    second step of project

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,11 +1,13 @@
 import argparse
 
+
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument('integers', metavar='first_file', type=int)
-    parser.add_argument('integers', metavar='second_file', type=int)
+    parser.add_argument('integers', metavar='first_file', type=str)
+    parser.add_argument('integers', metavar='second_file', type=str)
+    parser.add_argument('-f', '--format', metavar='FORMAT', help='set format of output')
     args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    print(args.accamulate(args.integers))
 
 
 if __name__ == '__main__':
