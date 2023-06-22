1bdeecd7339859b54d896a24cf581770185a6ee5
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-06-22 22:20:38 +0200

    idk i hate this piece of code

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -10,7 +10,6 @@ def gendiff():
     parser.add_argument('integers', metavar='second_file', type=str)
     parser.add_argument('-f', '--format',
                         metavar='FORMAT', help='set format of output')
-    args = parser.parse_args()
 
 
 def main():
@@ -26,22 +25,22 @@ def generate_diff(file_path1, file_path2):
     sorted_tuples = sorted(new_file.items(), key=lambda item: item[0])
     sorted_dict = {k: v for k, v in sorted_tuples}
     result = '{\n'
+
     for k in sorted_dict.keys():
         if k in file_1 and k in file_2:
-            v_1 = str(file_1[k]).lower()
-            v_2 = str(file_2[k]).lower()
             if file_1[k] == file_2[k]:
-                result += f'    {k}: {v_1}\n'
+                result += f'    {k}: {file_1[k]}\n'
             elif file_1[k] != file_2[k]:
-                result += f'  - {k}: {v_1}\n'
-                result += f'  + {k}: {v_2}\n'
+                result += f'  - {k}: {file_1[k]}\n'
+                result += f'  + {k}: {file_2[k]}\n'
+
         elif k in file_1 and k not in file_2:
-            v_1 = str(file_1[k]).lower()
-            result += f'  - {k}: {v_1}\n'
+            result += f'  - {k}: {file_1[k]}\n'
+
         elif k not in file_1 and k in file_2:
-            v_2 = str(file_2[k]).lower()
-            result += f'  + {k}: {v_2}\n'
-    return result + '}'
+            result += f'  + {k}: {file_2[k]}\n'
+
+    return str(result).lower() + '}'
 
 
 if __name__ == '__main__':
--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -1,12 +1,12 @@
-from gendiff.scripts.gendiff import generate_diff
+from gendiff.generate_diff import generate_diff
 import pytest
 
 
 @pytest.fixture
 def test_generate_diff():
-    file1 = 'file1.json'
-    file2 = 'file2.json'
+    file1 = 'tests/fixtures/file1.json'
+    file2 = 'tests/fixtures/file2.json'
     test_file = generate_diff(file1, file2)
-    assert isinstance(str, generate_diff(file1, file2))
+    assert isinstance(str, test_file)
     assert test_file.startswith('{')
     assert test_file.endswith('}')
