99c8c20ec330c531f332ecbb52ea315012faf7e9
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-07-13 20:22:26 +0200

    last stage of project fightinh with tests

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -9,12 +9,9 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
     data1 = parser(file_path1)
     data2 = parser(file_path2)
     formatted_data = create_and_get_diff(data1, data2)
-    if format_name == 'stylish':
-        return make_stylish(formatted_data)
-    elif format_name == 'plain':
+    if format_name == 'plain':
         return make_plain(formatted_data)
     elif format_name == 'json':
         return make_json(formatted_data)
     else:
-        return f'Please enter one of these formats:\n' \
-               f'stylish\nplain\njson'
+        return make_stylish(formatted_data)
