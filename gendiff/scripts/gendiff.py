538a53febeff3edb41d71ac6db9eaf54c320deb6
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-07-16 20:16:36 +0200

    tryin to complete hexlet tests p6

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 import argparse
-from gendiff.generate_diff import generate_diff
+from gendiff.parser import parser
+from gendiff.get_diff import create_and_get_diff
+from gendiff.formaters.stylish import make_stylish
+from gendiff.formaters.plain import make_plain
+from gendiff.formaters.JSON import make_json
+
+
+def generate_diff(file_path1, file_path2, format_name='stylish'):
+    data1 = parser(file_path1)
+    data2 = parser(file_path2)
+    formatted_data = create_and_get_diff(data1, data2)
+    if format_name == 'plain':
+        return make_plain(formatted_data)
+    elif format_name == 'json':
+        return make_json(formatted_data)
+    else:
+        return make_stylish(formatted_data)
 
 
 def main():
