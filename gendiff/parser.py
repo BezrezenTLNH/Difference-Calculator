ac6d33eab9e5135824bceeccfc029535b67e5e16
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-07-17 07:50:54 +0200

    fix some bugs

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -5,6 +5,7 @@ import yaml
 def parser(path):
     if path.endswith('.yml') or path.endswith('.yaml'):
         data = yaml.safe_load(open(path))
+
     elif path.endswith('.json'):
         data = json.load(open(path))
     return data
