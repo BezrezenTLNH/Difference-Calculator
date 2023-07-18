84318e0137cb1e848c414be8998f110ea1004a6b
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-07-18 12:03:52 +0200

    separate parsins system

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,5 +1,6 @@
 import json
 import yaml
+import argparse
 
 
 def parser(path):
@@ -9,3 +10,16 @@ def parser(path):
     elif path.endswith('.json'):
         data = json.load(open(path))
     return data
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file', metavar='first_file', type=str)
+    parser.add_argument('second_file', metavar='second_file', type=str)
+    parser.add_argument(
+        '-f', '--format', metavar='FORMAT',
+        help='set format of output', default='stylish')
+    args = parser.parse_args()
+    return args
--- gendiff/scripts/diff.py
+++ gendiff/scripts/diff.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
-import argparse
 from gendiff.gendiff import generate_diff
+from gendiff.parser import parse_args
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument('first_file', metavar='first_file', type=str)
-    parser.add_argument('second_file', metavar='second_file', type=str)
-    parser.add_argument(
-        '-f', '--format', metavar='FORMAT',
-        help='set format of output', default='stylish')
-    args = parser.parse_args()
+    args = parse_args()
     print(generate_diff(args.first_file, args.second_file, args.format))
 
 
